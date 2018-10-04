52c80f3f7f161e01edec6cc47d09985bb39d9a25
Irwin <Collen.Irwin@clinton.edu>
2018-10-04 15:29:22 -0400

    Added docstrings to WatchedFileComparer

--- FileMonitorConsole/WatchedFileComparer.cs
+++ FileMonitorConsole/WatchedFileComparer.cs
@@ -2,8 +2,16 @@
 
 namespace FileMonitorConsole
 {
+    /// <summary>
+    /// IEqaulityComparer for <see cref="WatchedFile"/> objects,
+    /// compares each object by their File's FullName (full path)
+    /// </summary>
     public class WatchedFileComparer : IEqualityComparer<WatchedFile>
     {
+        /// <summary>
+        /// Compares two <see cref="WatchedFile"/> objects a and b,
+        /// returning true both share the same path
+        /// </summary>
         public bool Equals(WatchedFile a, WatchedFile b)
         {
             return a.File.FullName == b.File.FullName;
